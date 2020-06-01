e42ed174a805004eb14cc25e7d843923216cddc2
Stephan Gerbeth <stephan.gerbeth@grabarzundpartner.de>
2020-06-01 20:10:32 +0200

    feat(font-face): dynamic generation of font face declarations

--- example/layouts/default.vue
+++ example/layouts/default.vue
@@ -21,47 +21,4 @@ body {
   font-style: var(--font-style);
   font-weight: var(--font-weight);
 }
-
-@font-face {
-  font-family: 'Comic Neue';
-  font-style: normal;
-  font-weight: 300;
-  src: url('~assets/fonts/comic-neue-v1-latin/comic-neue-v1-latin-300.woff2') format('woff2');
-}
-
-@font-face {
-  font-family: 'Comic Neue';
-  font-style: italic;
-  font-weight: 300;
-  src: url('~assets/fonts/comic-neue-v1-latin/comic-neue-v1-latin-300italic.woff2') format('woff2');
-}
-
-@font-face {
-  font-family: 'Comic Neue';
-  font-style: normal;
-  font-weight: 400;
-  src: url('~assets/fonts/comic-neue-v1-latin/comic-neue-v1-latin-regular.woff2') format('woff2');
-}
-
-@font-face {
-  font-family: 'Comic Neue';
-  font-style: italic;
-  font-weight: 400;
-  src: url('~assets/fonts/comic-neue-v1-latin/comic-neue-v1-latin-italic.woff2') format('woff2');
-}
-
-@font-face {
-  font-family: 'Comic Neue';
-  font-style: normal;
-  font-weight: 700;
-  src: url('~assets/fonts/comic-neue-v1-latin/comic-neue-v1-latin-700.woff2') format('woff2');
-}
-
-@font-face {
-  font-family: 'Comic Neue';
-  font-style: italic;
-  font-weight: 700;
-  src: url('~assets/fonts/comic-neue-v1-latin/comic-neue-v1-latin-700italic.woff2') format('woff2');
-}
-
 </style>
