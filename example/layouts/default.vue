773f6b57835f2ad743a903e749f4888309115b3a
Stephan Gerbeth <stephan.gerbeth@grabarzundpartner.de>
2020-06-02 09:51:48 +0200

    fix(css-var-declaration): embedded into module - removed from layout/default

--- example/layouts/default.vue
+++ example/layouts/default.vue
@@ -11,14 +11,7 @@ export default {
 </script>
 
 <style lang="postcss">
-
 body {
   margin: 0;
-}
-
-.font {
-  font-family: var(--font-family, Arial);
-  font-style: var(--font-style);
-  font-weight: var(--font-weight);
 }
 </style>
