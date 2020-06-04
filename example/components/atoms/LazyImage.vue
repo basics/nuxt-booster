aa9d2fc72229ae4d1bb1e87141d65e3999f83557
Stephan Gerbeth <stephan.gerbeth@grabarzundpartner.de>
2020-06-04 16:05:07 +0200

    fix(lazy-image): embedded image size cache

--- example/components/atoms/LazyImage.vue
+++ example/components/atoms/LazyImage.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-import timeoutFetch from '@/utils/timeoutFetch'
+import { getImageSize } from '@/utils/image'
 global.IntersectionObserver = global.IntersectionObserver || class { observe () {}; unobserve () {}}
 
 export default {
@@ -66,6 +66,8 @@ export default {
   created () {
     if (!this.$options.critical) {
       this.observer = new global.IntersectionObserver(() => this.load(), { threshold: 0 })
+    } else {
+      getImageSize(this.src)
     }
   },
 
@@ -98,31 +100,6 @@ export default {
       }
     }
   }
-}
-
-async function getImageSize (url) {
-  if (process.server) {
-    try {
-      const sizeOf = (await import('buffer-image-size')).default
-      const buffer = await getBuffer(url)
-      return sizeOf(buffer)
-    } catch (e) {
-      return {}
-    }
-  } else {
-    return new Promise((resolve) => {
-      const img = new Image()
-      img.onload = () => resolve({ width: img.width, height: img.height })
-      img.src = url
-    })
-  }
-}
-
-async function getBuffer (url) {
-  const result = await timeoutFetch(url, { method: 'GET' })
-  const blob = await result.blob()
-  const arrayBuffer = await blob.arrayBuffer()
-  return Buffer.from(arrayBuffer)
 }
 </script>
 
