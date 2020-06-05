f2b9cf3db3367eb6251c108bb434e5ddae3be4d9
StephanGerbeth <stephan.gerbeth@gmail.com>
2020-06-05 12:01:36 +0200

    fix(lazy-image): refactoring

---
+++ lib/components/CustomImage.vue
@@ -0,0 +1,51 @@
+<template>
+  <img
+    class="custom-image"
+    :src="src"
+    :srcset="srcset"
+    :width="size.width"
+    :height="size.height"
+    v-bind="$attrs"
+    loading="lazy"
+    @load="onLoad"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default () {
+        return null
+      }
+    },
+
+    srcset: {
+      type: String,
+      default () {
+        return null
+      }
+    },
+
+    size: {
+      type: Object,
+      default () {
+        return { width: null, height: null }
+      }
+    }
+  },
+
+  methods: {
+    onLoad (e) {
+      this.$emit('load', e.target)
+    }
+  }
+}
+</script>
+
+<style lang="postcss" type="flow" scoped>
+.custom-image {
+  display: block;
+}
+</style>
