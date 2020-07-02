41846393b61eb8c6aa972adc9219910ed69ad6a0
StephanGerbeth <stephan.gerbeth@gmail.com>
2020-07-02 19:57:46 +0200

    fix(img): additional srcset images

--- example/pages/critical/lazy-image-srcset.vue
+++ example/pages/critical/lazy-image-srcset.vue
@@ -5,7 +5,7 @@
         <LazyImage v-bind="imageA" />
       </template>
       <template v-slot:title>
-        <p>Critical<br>LazyImage - SRCSET<br>Image Switch by >768px</p>
+        <p>Critical<br>LazyImage - SRCSET<br>Image Switch by >480 &amp; >768px</p>
       </template>
     </organism-preview-container>
   </div>
@@ -22,14 +22,27 @@ export default {
         title: 'Title Text',
         srcset: [
           {
+            url: 'img/critical-1200.jpg',
+            width: 1200
+          },
+          {
             url: 'img/critical-768.jpg',
             width: 768
           },
           {
-            url: 'img/critical-1200.jpg',
-            width: 2400
+            url: 'img/critical-960.jpg',
+            width: 960
+          },
+          {
+            url: 'img/critical-480.jpg',
+            width: 480
           }
         ]
+        // sizes: [
+        //   '(min-width: 768px) 1200px',
+        //   '(min-width: 480px) 768px',
+        //   '100vw'
+        // ]
       }
     }
   }
