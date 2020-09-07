838685f9a6956cc9181894d756dc1f80a3ae29a3
Stephan Gerbeth <stephan.gerbeth@grabarzundpartner.de>
2020-09-07 16:52:07 +0200

    fix(webpack): removed lazy comments

--- example/pages/test.vue
+++ example/pages/test.vue
@@ -22,19 +22,19 @@ import { hydrateWhenVisible } from 'vue-lazy-hydration'
 
 export default {
   components: {
-    test0: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest'), { observerOptions: { rootMargin: '0px' } }),
-    test1: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest1'), { observerOptions: { rootMargin: '0px' } }),
-    test2: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest2'), { observerOptions: { rootMargin: '0px' } }),
-    test3: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest3'), { observerOptions: { rootMargin: '0px' } }),
-    test4: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest4'), { observerOptions: { rootMargin: '0px' } }),
-    test5: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest5'), { observerOptions: { rootMargin: '0px' } }),
-    test6: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest6'), { observerOptions: { rootMargin: '0px' } }),
-    test7: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest7'), { observerOptions: { rootMargin: '0px' } }),
-    test8: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest8'), { observerOptions: { rootMargin: '0px' } }),
-    test9: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest9'), { observerOptions: { rootMargin: '0px' } }),
-    test10: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest10'), { observerOptions: { rootMargin: '0px' } }),
-    test11: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest11'), { observerOptions: { rootMargin: '0px' } }),
-    test12: hydrateWhenVisible(() => import(/* webpackMode: "lazy" */'@/components/atoms/HydrateTest12'), { observerOptions: { rootMargin: '0px' } })
+    test0: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest'), { observerOptions: { rootMargin: '0px' } }),
+    test1: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest1'), { observerOptions: { rootMargin: '0px' } }),
+    test2: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest2'), { observerOptions: { rootMargin: '0px' } }),
+    test3: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest3'), { observerOptions: { rootMargin: '0px' } }),
+    test4: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest4'), { observerOptions: { rootMargin: '0px' } }),
+    test5: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest5'), { observerOptions: { rootMargin: '0px' } }),
+    test6: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest6'), { observerOptions: { rootMargin: '0px' } }),
+    test7: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest7'), { observerOptions: { rootMargin: '0px' } }),
+    test8: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest8'), { observerOptions: { rootMargin: '0px' } }),
+    test9: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest9'), { observerOptions: { rootMargin: '0px' } }),
+    test10: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest10'), { observerOptions: { rootMargin: '0px' } }),
+    test11: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest11'), { observerOptions: { rootMargin: '0px' } }),
+    test12: hydrateWhenVisible(() => import('@/components/atoms/HydrateTest12'), { observerOptions: { rootMargin: '0px' } })
   },
 
   data () {
