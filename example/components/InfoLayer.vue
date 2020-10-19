6cf004dcc179de0a10c601b84ec767c8694e3371
Stephan Gerbeth <stephan.gerbeth@grabarzundpartner.de>
2020-10-19 18:15:23 +0200

    fix(info-layer): modified info layer

--- example/components/InfoLayer.vue
+++ example/components/InfoLayer.vue
@@ -26,34 +26,45 @@ export default {
   left: 0;
   z-index: 1000;
   width: 100%;
-  color: #f5f7fa;
-  background-color: #2c3e50;
-  box-shadow: 0 0 10px rgba(255, 255, 255, 0.6);
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.25);
   opacity: 0;
-  transform: translateY(-50%);
-
-  @media (prefers-color-scheme: dark) {
-    color: #2f495e;
-    background-color: #fff;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
-  }
+  animation-name: fade-in;
+  animation-duration: 0.2s;
+  animation-delay: 0.5s;
+  backdrop-filter: blur(7px);
+  animation-fill-mode: forwards;
 
   & > div {
     padding: 10px;
     font-family: sans-serif;
     font-weight: bold;
+    color: #f5f7fa;
     text-align: center;
-  }
+    background-color: #2c3e50;
+    box-shadow: 0 0 10px rgba(255, 255, 255, 0.6);
+    transform: translateY(-50%);
+    animation-name: fall-down;
+    animation-duration: 0.2s;
+    animation-delay: 0.5s;
+    animation-fill-mode: forwards;
 
-  animation-name: fade-in;
-  animation-duration: 0.2s;
-  animation-delay: 0.5s;
-  animation-fill-mode: forwards;
+    @media (prefers-color-scheme: dark) {
+      color: #2f495e;
+      background-color: #fff;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
+    }
+  }
 }
 
 @keyframes fade-in {
   100% {
     opacity: 1;
+  }
+}
+
+@keyframes fall-down {
+  100% {
     transform: translateY(0%);
   }
 }
