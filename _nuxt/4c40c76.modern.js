ff635c2a61692e8608c1744084e5a84f689d721d
ThornWalli <ThornWalli@users.noreply.github.com>
2020-10-20 14:06:28 +0000

    deploy: f635134b5973ec285618c4b6bdf129f6d4fffc19

---
+++ _nuxt/4c40c76.modern.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{213:function(d,j,n){"use strict";n.r(j);var e={mounted(){console.log("TEST MOUNTED")}},f=n(3),component=Object(f.a)(e,(function(){var d=this.$createElement;return(this._self._c||d)("div",[this._v("juru mjdkfjdkfjd jfkdjfkdjkfjdkfj dkjfkdjfkdjkfjdkfj")])}),[],!1,null,null,null);j.default=component.exports}}]);
