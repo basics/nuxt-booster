006a18c7a10c59789c75168e7586f0b21862c55c
ThornWalli <ThornWalli@users.noreply.github.com>
2021-01-03 11:12:16 +0000

    deploy: 044fb265663e71989412d67248f62e380f4dd089

--- docs/_nuxt/static/1608558590/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fv-font","\u002Fsetup","\u002Fcomponents\u002Flazy-picture","\u002Fusage","\u002Fcomponents\u002Flazy-iframe","\u002Fcomponents\u002Flazy-image"]})
