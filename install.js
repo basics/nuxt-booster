6621884d90b5acfe5acf93447c73f122542c4897
Thorn Walli <thorn.walli@grabarzundpartner.de>
2021-01-08 17:52:04 +0100

    chore(test): test 2

--- install.js
+++ install.js
@@ -1,108 +1,28 @@
 const os = require('os')
 const path = require('path')
-const fs = require('fs')
 const fsExtra = require('fs-extra')
-console.log(fsExtra)
-function install () {
-  const isPackage = path.basename(path.join(process.cwd(), '../')) === 'node_modules'
 
-  if (isPackage) {
-    const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'publish-'))
+const isPackage = path.basename(path.join(process.cwd(), '../')) === 'node_modules'
 
-    // collect files for clean package
-    const libDir = './lib'
-    const files = [
-      './index.js',
-      './package.json',
-      './README.md',
-      './LICENSE'
+if (isPackage) {
+  const tmpDir = fsExtra.mkdtempSync(path.join(os.tmpdir(), 'publish-'))
 
-    ].concat(fs.readdirSync(libDir).map(file => `./lib/${file}`))
+  // collect files for clean package
+  const libDir = './lib'
+  const files = [
+    './index.js',
+    './package.json',
+    './README.md',
+    './LICENSE',
+    './node_modules'
 
-    // copy files to tmp dir
-    mkdir(path.join(tmpDir, libDir))
-    files.forEach((file) => {
-      const filename = file
-      const stat = fs.statSync(filename)
+  ].concat(fsExtra.readdirSync(libDir).map(file => `./lib/${file}`))
 
-      if (stat.isDirectory()) {
-        copyDir(file, path.join(tmpDir, file))
-      } else {
-        fs.copyFileSync(file, path.join(tmpDir, file))
-      }
-    })
+  // copy files to tmp dir
+  files.forEach(file => fsExtra.copySync(file, path.join(tmpDir, file)))
 
-    // delete all files in package
-    fs.readdirSync('.').forEach((file) => {
-      const filename = file
-      const stat = fs.statSync(filename)
+  // // delete all files in package
+  fsExtra.readdirSync('.').map(file => fsExtra.rmSync(file))
 
-      if (stat.isDirectory()) {
-        rmdir(file)
-      } else {
-        fs.unlinkSync(file)
-      }
-    })
-
-    copyDir(tmpDir, process.cwd())
-  }
+  fsExtra.copySync(tmpDir, process.cwd())
 }
-
-function mkdir (dir) {
-  // making directory without exception if exists
-  try {
-    fs.mkdirSync(dir)
-  } catch (e) {
-    if (e.code !== 'EEXIST') {
-      throw e
-    }
-  }
-};
-
-function rmdir (dir) {
-  if (fs.existsSync(dir)) {
-    const list = fs.readdirSync(dir)
-    for (let i = 0; i < list.length; i++) {
-      const filename = path.join(dir, list[i])
-      const stat = fs.statSync(filename)
-
-      if (filename === '.' || filename === '..') {
-        // pass these files
-      } else if (stat.isDirectory()) {
-        // rmdir recursively
-        rmdir(filename)
-      } else {
-        // rm fiilename
-        fs.unlinkSync(filename)
-      }
-    }
-    fs.rmdirSync(dir)
-  } else {
-    console.warn('warn: ' + dir + ' not exists')
-  }
-};
-
-function copyDir (src, dest) {
-  console.log('copyDir', src, dest)
-  mkdir(dest)
-  const files = fs.readdirSync(src)
-  for (let i = 0; i < files.length; i++) {
-    const current = fs.lstatSync(path.join(src, files[i]))
-    if (current.isDirectory()) {
-      copyDir(path.join(src, files[i]), path.join(dest, files[i]))
-    } else if (current.isSymbolicLink()) {
-      const symlink = fs.readlinkSync(path.join(src, files[i]))
-      fs.symlinkSync(symlink, path.join(dest, files[i]))
-    } else {
-      copy(path.join(src, files[i]), path.join(dest, files[i]))
-    }
-  }
-};
-
-function copy (src, dest) {
-  const oldFile = fs.createReadStream(src)
-  const newFile = fs.createWriteStream(dest)
-  oldFile.pipe(newFile)
-};
-
-install()
