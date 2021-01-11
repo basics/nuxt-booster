5ce5d1df9a693fa93822d5c6bbd05f98c3f7dd83
Thorn Walli <thorn.walli@grabarzundpartner.de>
2021-01-11 10:21:15 +0100

    chore(install): test 2

--- install.js
+++ install.js
@@ -1,12 +1,10 @@
-const os = require('os')
+
 const path = require('path')
 const fsExtra = require('fs-extra')
 
 const isPackage = path.basename(path.join(process.cwd(), '../')) === 'node_modules'
 
 if (isPackage) {
-  const tmpDir = fsExtra.mkdtempSync(path.join(os.tmpdir(), 'publish-'))
-
   const ignoredFiles = [
     'index.js',
     'package.json',
@@ -20,7 +18,7 @@ if (isPackage) {
   // // exclude lib dir
   const libDir = path.resolve(__dirname, './lib')
   const libFiles = fsExtra.readdirSync(libDir).map(file => path.resolve(libDir, file))
-  libFiles.forEach(file => fsExtra.existsSync(file) && fsExtra.copySync(file, path.join(tmpDir, path.relative(libDir, file))))
+  libFiles.forEach(file => fsExtra.existsSync(file) && fsExtra.copySync(file, path.join(process.cwd(), path.relative(libDir, file))))
 
   // // delete all files in package
   // fsExtra.readdirSync('.').forEach(file => fsExtra.removeSync(file))
