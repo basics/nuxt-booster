1a430c7b5c5b5c6bc2f391d06729158b47544024
Stephan Gerbeth <stephan.gerbeth@grabarzundpartner.de>
2021-01-19 23:08:15 +0100

    fix(picture): cleanup & restructuring

--- lib/components/experimental/ExperimentalPicture.vue
+++ lib/components/experimental/ExperimentalPicture.vue
@@ -32,6 +32,7 @@ import { createSVGPlaceholder, createURLPlaceholder } from 'nuxt-speedkit/utils/
 import { registerIntersecting, unregisterIntersecting } from 'nuxt-speedkit/utils/intersectionObserver'
 import CustomNoScript from 'nuxt-speedkit/components/customs/CustomNoScript'
 import { isWebpSupported, isPreloadSupported } from 'nuxt-speedkit/utils/support'
+import { getPreloadDescription, doPreloadFallback } from 'nuxt-speedkit/utils/preloadNew'
 
 export default {
   components: {
@@ -87,12 +88,13 @@ export default {
 
   head () {
     if (this.isCritical || (process.client & this.visible)) {
+      const [source] = filterBySupportedMimeTypes(this.resolvedSources, this.webpSupport)
       if (isPreloadSupported()) {
         return {
-          link: getPreloadDescriptions(this.resolvedSources, this.onPreload, this.webpSupport)
+          link: [getPreloadDescription(source, this.crossorigin, this.onPreload)]
         }
       } else {
-        this.onPreload()
+        doPreloadFallback(source, this.crossorigin, this.onPreload)
       }
     }
   },
@@ -139,36 +141,25 @@ export default {
     },
 
     getSources () {
-      return [
-        this.sources.map(source => this.$img.sizes(source.src, source.sizes, { format: 'webp' })).flat(),
-        this.sources.map(source => this.$img.sizes(source.src, source.sizes, { format: 'jpeg' })).flat()
-      ].map((sources) => {
+      return ['webp', 'jpeg'].map((format) => {
+        return this.sources.map(source => this.$img.sizes(source.src, source.sizes, { format })).flat()
+      }).map((sources) => {
         return {
           srcset: sources.map(({ width, url }) => width ? `${url} ${width}w` : url).join(', '),
           sizes: sources.map(({ width, media }) => media ? `${media} ${width}px` : `${width}px`).reverse().join(', '),
-          type: `image/${sources[0].format}`
+          type: getMimeType(sources[0].format)
         }
       })
     }
   }
 }
 
-function getPreloadDescriptions (sources, callback = () => {}, webpSupport) {
-  const [source] = sources.reduce((result, source) => {
-    if (source.type !== 'image/webp' || (source.type === 'image/webp' && webpSupport)) {
-      result.push(source)
-    }
-    return result
-  }, [])
+function getMimeType (format) {
+  return `image/${format}`
+}
 
-  return [{
-    rel: 'preload',
-    as: 'image',
-    crossorigin: 'anonymous',
-    imageSrcset: source.srcset,
-    imageSizes: source.sizes,
-    callback
-  }]
+function filterBySupportedMimeTypes (sources, webpSupport) {
+  return sources.filter(source => source.type !== getMimeType('webp') || (source.type === getMimeType('webp') && webpSupport))
 }
 </script>
 
