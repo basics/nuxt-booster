d879c6496cf16ea6b742474e38d8dd9315adc87c
StephanGerbeth <StephanGerbeth@users.noreply.github.com>
2021-01-22 14:58:29 +0000

    deploy: 9e79522462a45bb2a3a08cb6049bdf57edcae404

---
+++ docs/_nuxt/static/1611327353/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fusage","\u002Fcomponents\u002Fspeedkit-picture","\u002Fcomponents\u002Fspeedkit-image","\u002Fsetup","\u002Fv-font","\u002Fcomponents\u002Fspeedkit-iframe"]})
