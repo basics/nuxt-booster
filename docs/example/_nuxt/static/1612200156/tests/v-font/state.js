9cfb4647411cbd16910f15a400d3ab6cf7b69c34
ThornWalli <ThornWalli@users.noreply.github.com>
2021-02-01 17:29:22 +0000

    deploy: f9eb5e5c7c37cd049221a5c81c8dfda4d6b2b0b8

--- docs/example/_nuxt/static/1612200156/tests/v-font/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1612200156",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftests\u002Fv-font",config:{app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
