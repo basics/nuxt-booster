9e7c13459b45d74804bd69b9c641f32dd647a7dd
StephanGerbeth <StephanGerbeth@users.noreply.github.com>
2021-02-03 07:27:43 +0000

    deploy: 01b45ac3cd3bd30cf84c09a15b06d221908de3fd

--- docs/example/_nuxt/static/1612286934/experimental/experimental-empty-large/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1612286934",layout:"default",error:a,serverRendered:true,routePath:"\u002Fexperimental\u002Fexperimental-empty-large",config:{app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ docs/example/_nuxt/static/1612337196/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1612337196",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
