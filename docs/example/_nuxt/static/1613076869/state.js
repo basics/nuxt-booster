03b09a581871e93e16f772d0e0d5afad6d4b864e
ThornWalli <ThornWalli@users.noreply.github.com>
2021-02-11 22:40:33 +0000

    deploy: 01d25ac87e8972f0f884245980a2dbd3331b38a7

--- docs/example/_nuxt/static/1613076869/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1613076869",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
