05b518d6c1952f493727a03e824b38ef56b463ec
ThornWalli <ThornWalli@users.noreply.github.com>
2021-02-12 17:02:38 +0000

    deploy: f1d98a8f3f5edaf61e8160eb62ce3fef8ae3cd06

--- docs/example/_nuxt/static/1613144715/tests/speedkit-youtube/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1613144715",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftests\u002Fspeedkit-youtube",config:{app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
