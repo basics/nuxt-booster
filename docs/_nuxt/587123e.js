6962c94cf6360d2e60f5b0629e22e3544b753e7d
ThornWalli <ThornWalli@users.noreply.github.com>
2021-02-18 08:15:17 +0000

    deploy: 614f110d34b22b645005ad27e23c7e7eb0a210eb

--- docs/_nuxt/587123e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{166:function(e,n,t){"use strict";t.r(n),n.default={search:{placeholder:"Belgelerde arayın (Odaklanmak için tıklayınız)"},toc:{title:"Bu sayfada"},article:{github:"Bu sayfayı GitHub üzerinde düzenleyin"}}}}]);
