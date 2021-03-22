193ec03ea189a0aab94403551e17e3cdc492b895
Thorn Walli <thorn.walli@grabarzundpartner.de>
2021-03-22 18:51:29 +0100

    docs(content): update example link

--- docs/components/AsideBottom.vue
+++ docs/components/AsideBottom.vue
@@ -37,14 +37,8 @@ export default {
         More: [
           {
             icon: 'IconExternalLink',
-            url: 'https://github.com/GrabarzUndPartner/nuxt-speedkit',
-            title: 'Example Project',
-            target: '_blank'
-          },
-          {
-            icon: 'IconExternalLink',
-            url: 'https://nuxt-speedkit.grabarzundpartner.dev/example',
-            title: 'Live Example',
+            url: 'https://github.com/GrabarzUndPartner/nuxt-speedkit-example',
+            title: 'Example',
             target: '_blank'
           }
         ]
