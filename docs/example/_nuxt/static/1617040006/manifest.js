856b96c2907a49112f8cd132da5f8de2ccb580d4
ThornWalli <ThornWalli@users.noreply.github.com>
2021-03-29 17:50:09 +0000

    deploy: 2b252f9a696d4af6312b4c388cbc2bd7539977d1

---
+++ docs/example/_nuxt/static/1617040006/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ftests\u002Fspeedkit-iframe","\u002Ftests\u002Fspeedkit-picture","\u002Ftests\u002Fspeedkit-youtube","\u002Ftests\u002Fv-font","\u002Ftests\u002Fv-font-media","\u002Fexperimental\u002Fspeedkit-youtube","\u002Fexperimental","\u002Fexperimental\u002Fspeedkit-picture\u002F5"]})
