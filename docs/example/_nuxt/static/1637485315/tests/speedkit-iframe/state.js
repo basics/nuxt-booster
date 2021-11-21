3fcbe2f501cec6e46946e01e6907360d28dc2871
StephanGerbeth <StephanGerbeth@users.noreply.github.com>
2021-11-21 09:03:15 +0000

    deploy: 3d4567f6eb205529af66af5e486e120a2b5fcbb5

---
+++ docs/example/_nuxt/static/1637485315/tests/speedkit-iframe/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1637485315",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftests\u002Fspeedkit-iframe",config:{_app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
