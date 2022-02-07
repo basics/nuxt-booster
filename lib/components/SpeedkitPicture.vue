0316be6e094124864e6336c66fdb7a35cbe519c6
Thorn Walli <lammpee@gmail.com>
2022-02-07 14:14:01 +0100

    chore(components): components cleaned up

--- lib/components/SpeedkitPicture.vue
+++ lib/components/SpeedkitPicture.vue
@@ -1,144 +1,26 @@
-<template>
-  <picture :class="classNames.picture" class="nuxt-speedkit-picture">
-    <picture-source v-for="(source) in formatSources" :key="source.key" :source="source" />
-    <base-image :class="classNames.image" :title="title" :alt="alt" :loading-spinner="loadingSpinner" @load="onLoad" />
-  </picture>
-</template>
-
 <script>
-import BaseImage from 'nuxt-speedkit/components/SpeedkitImage';
-import SourceList from 'nuxt-speedkit/components/SpeedkitPicture/classes/SourceList';
-import LoadingSpinner from 'nuxt-speedkit/components/SpeedkitImage/classes/LoadingSpinner';
-import PictureSource from 'nuxt-speedkit/components/SpeedkitPicture/Source';
-
-const formatPriority = ['avif', 'webp', 'png', 'jpg'];
+import LazyHydrate from 'vue-lazy-hydration';
+import SpeedkitPicture from 'nuxt-speedkit/components/SpeedkitPicture/Base';
 
 export default {
-  components: {
-    PictureSource,
-    BaseImage
-  },
+  inheritAttrs: false,
 
   props: {
-    sources: {
-      type: [Array, SourceList],
-      required: true
-    },
-
-    formats: {
-      type: Array,
-      default () {
-        return ['webp', 'avif', 'jpg'];
-      }
-    },
-
-    loadingSpinner: {
-      type: LoadingSpinner,
-      default: undefined
-    },
-
-    title: {
-      type: String,
-      default: null
-    },
-
-    alt: {
-      type: String,
-      default: null
+    hydrate: {
+      type: Boolean,
+      default: true
     }
   },
 
-  data () {
-    return {
-      metaSources: {},
-      classNames: {}
-    };
-  },
-
-  async fetch () {
-    const { ssrContext } = this.$nuxt.context;
-    this.metaSources = await this.sourceList.getMeta(this.$img, ssrContext);
-    this.classNames = this.metaSources.classNames;
-  },
-
-  head () {
-    return {
-      style: this.style
-    };
-  },
-
-  fetchKey () {
-    return `picture-${this.sourceList.key}`;
-  },
-
-  computed: {
-    sourceList () {
-      return SourceList.create(this.sources);
-    },
-    formatSources () {
-      const sortedFormatsByPriority = formatPriority.filter(v => this.formats.includes(v));
-      const preloadFormat = formatPriority.find(v => this.formats.includes(v));
-      return this.sourceList.getFormats(sortedFormatsByPriority, preloadFormat, this.isCritical);
-    },
-
-    style () {
-      if (!this.metaSources) {
-        return [];
-      }
-      const metaSources = (this.metaSources.length && new SourceList(this.metaSources)) || this.metaSources;
-      return [{ hid: this.classNames.picture, type: 'text/css', cssText: metaSources.style }];
+  render (h) {
+    if (!this.hydrate) {
+      return h(LazyHydrate, { props: { never: true } }, [
+        h('noscript', {}, [
+          h(SpeedkitPicture, { props: { ...this.$attrs, critical: this.hydrate, on: this.$listeners } })
+        ])
+      ]);
     }
-  },
-
-  methods: {
-    onLoad (e) {
-      this.$emit('load', e);
-    }
+    return h(SpeedkitPicture, { props: { ...this.$attrs, critical: this.isCritical, on: this.$listeners } });
   }
 };
 </script>
-
-<style lang="postcss" scoped>
-/*! purgecss start ignore */
-
-.nuxt-speedkit-picture {
-  position: relative;
-  box-sizing: border-box;
-  display: block;
-  width: 100%;
-
-  & img {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    box-sizing: border-box;
-  }
-
-  &::before {
-    position: relative;
-    box-sizing: border-box;
-    display: block;
-    content: "";
-  }
-
-  @supports (aspect-ratio: 1) {
-    position: unset;
-
-    & img {
-      position: unset;
-      top: unset;
-      right: unset;
-      bottom: unset;
-      left: unset;
-    }
-
-    &::before {
-      display: none;
-    }
-  }
-}
-
-/*! purgecss end ignore */
-</style>
