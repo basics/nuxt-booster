2934c667c13d0dbdd9d8844ad4891017fd517d0d
StephanGerbeth <StephanGerbeth@users.noreply.github.com>
2022-02-08 22:24:04 +0000

    deploy: d27b1b27a100b2c57a194a90bf5327b4f67cc056

--- docs/example/_nuxt/static/1644344845/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1644344845",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
