498f5097072faec67c934ff72b65c6e4e6bd8154
StephanGerbeth <StephanGerbeth@users.noreply.github.com>
2022-02-15 18:16:37 +0000

    deploy: efeef41477605697ee979ae2ced2c9cb545178f8

--- docs/example/_nuxt/static/1644791489/experimental/speedkit-youtube/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1644791489",layout:"default",error:a,serverRendered:true,routePath:"\u002Fexperimental\u002Fspeedkit-youtube",config:{_app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
