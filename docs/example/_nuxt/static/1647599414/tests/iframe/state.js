fca70d7ed5a854aa6e971a610e44904a1e887b76
ThornWalli <ThornWalli@users.noreply.github.com>
2022-03-18 10:33:21 +0000

    deploy: 8a52a2ef892a2e5f12e456ced3fc4b34c8b5daeb

---
+++ docs/example/_nuxt/static/1647599414/tests/iframe/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1647599414",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftests\u002Fiframe",config:{_app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
