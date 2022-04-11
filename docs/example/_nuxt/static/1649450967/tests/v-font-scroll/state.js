6a4dc1e2359c2076c97b481bd5ca26965a003c38
ThornWalli <ThornWalli@users.noreply.github.com>
2022-04-11 07:21:16 +0000

    deploy: 6caec3a7799a604fde7bd9ad125ede297f7ae7b6

--- docs/example/_nuxt/static/1649450967/tests/v-font-scroll/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1649450967",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftests\u002Fv-font-scroll",config:{_app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
