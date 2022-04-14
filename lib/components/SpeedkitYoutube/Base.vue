533170cba2a328c69e2995a30db9990624682ea3
Thorn Walli <lammpee@gmail.com>
2022-04-14 17:20:13 +0200

    fix(source): add fallback for missing src extension

--- lib/components/SpeedkitYoutube/Base.vue
+++ lib/components/SpeedkitYoutube/Base.vue
@@ -113,6 +113,7 @@ export default {
   computed: {
     pictureDataset () {
       return {
+        formats: this.$speedkit.pictureFormats,
         title: this.title,
         sources: [{
           src: `/youtube/vi/${this.videoId}/maxresdefault.jpg`,
