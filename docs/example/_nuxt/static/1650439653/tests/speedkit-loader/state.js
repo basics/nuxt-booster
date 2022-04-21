9511f2008851f394916ea387762abc372ab353a8
ThornWalli <ThornWalli@users.noreply.github.com>
2022-04-21 15:32:50 +0000

    deploy: 529aedc5d5f30ffa238a6b20e9f32a6c4299a07e

--- docs/example/_nuxt/static/1650439653/tests/speedkit-loader/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1650439653",layout:"blank",error:a,serverRendered:true,routePath:"\u002Ftests\u002Fspeedkit-loader",config:{_app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
