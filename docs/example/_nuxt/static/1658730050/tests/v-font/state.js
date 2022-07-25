1b1720e40315cd654ec9da3c2222261eee54509e
StephanGerbeth <StephanGerbeth@users.noreply.github.com>
2022-07-25 06:26:27 +0000

    deploy: 85f875fc39984243ecba41b7e2b9c8bd2397fe86

---
+++ docs/example/_nuxt/static/1658730050/tests/v-font/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1658730050",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftests\u002Fv-font",config:{_app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
