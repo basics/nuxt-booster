0e1847c3ad093ef6b1e2f1886c6e773cb9f5771c
StephanGerbeth <StephanGerbeth@users.noreply.github.com>
2022-08-06 08:21:56 +0000

    deploy: 3f54c12519cbe4ddd7880e2b37a34c6d57e4fbaa

---
+++ docs/example/_nuxt/static/1659773805/tests/speedkit-loader/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fexample\u002F_nuxt\u002Fstatic\u002F1659773805",layout:"blank",error:a,serverRendered:true,routePath:"\u002Ftests\u002Fspeedkit-loader",config:{_app:{basePath:"\u002Fexample\u002F",assetsPath:"\u002Fexample\u002F_nuxt\u002F",cdnURL:a}}}}(null));
