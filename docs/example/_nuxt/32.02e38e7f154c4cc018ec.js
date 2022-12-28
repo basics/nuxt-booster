82a2c4d7332e79606879fcc56db67ca04522890d
StephanGerbeth <StephanGerbeth@users.noreply.github.com>
2022-12-28 11:56:38 +0000

    deploy: 264eaa6293998bf9d289e153b766b94c2f457293

---
+++ docs/example/_nuxt/32.02e38e7f154c4cc018ec.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{541:function(e,o,l){"use strict";Object.defineProperty(o,"__esModule",{value:!0});const n=globalThis.fetch,t=globalThis.Headers,c=globalThis.Request,h=globalThis.Response;o.Headers=t,o.Request=c,o.Response=h,o.fetch=n}}]);
