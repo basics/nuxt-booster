05629ddc7915a928117f06faf28c345d881d716d
ThornWalli <ThornWalli@users.noreply.github.com>
2024-07-23 10:05:58 +0000

    deploy: 5300afb5a90d3f42163ee240c5ae7d787d23d94b

---
+++ playground/_nuxt/Bumktpza.js
@@ -0,0 +1,1 @@
+import{c as e,o as t}from"./CB3k6t9A.js";import"./2jZ54Mey.js";const _={__name:"index",setup(n){return(o,r)=>(t(),e("div",null,"blank"))}};export{_ as default};
---
+++ playground/_nuxt/CcOLHay0.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,A as t,o as n}from"./CB3k6t9A.js";import"./2jZ54Mey.js";const c={};function s(e,a){return n(),r("div",null,[t(e.$slots,"default")])}const f=o(c,[["render",s]]);export{f as default};
