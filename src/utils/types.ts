a666a2efd8443a0636e80431906b5c1a0a57a87d
ElPrudi <liampruditsch@gmail.com>
2025-03-11 10:07:15 +0100

    refactor: overhaul nuxt type generation

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,87 +1,63 @@
 import type FontConfig from '../classes/FontConfig';
+import type { FontOptionVariance } from '../types';
 
-export function getTypesContent(fontConfig: FontConfig) {
-  const family = unique(fontConfig.fonts.map(font => font.family));
-  const weight = unique(
-    fontConfig.fonts
-      .map(font => font.variances.map(variance => variance.weight))
-      .flat()
-  );
-  const style = unique(
-    fontConfig.fonts
-      .map(font => font.variances.map(variance => variance.style))
-      .flat()
-  );
-
-  return generateAliases({ family, weight, style });
-}
-
-function unique<T>(array: T[]) {
-  return Array.from(new Set(array));
-}
-
-function generateAliases({
-  family,
-  weight,
-  style
-}: {
+interface TextProperties {
+  [key: string]: (string | number)[]
   family: string[];
   weight: (string | number)[];
   style: string[];
-}) {
-  return `
-    export type FontFamily = ${family.map(family => `'${family}'`).join(' | ') || 'string'};
-    export type FontWeight = ${weight.map(v => (typeof v === 'number' ? Number(v) : `"${v}"`)).join(' | ') || 'string'};
-    export type FontStyle = ${style.map(style => `'${style}'`).join(' | ') || 'string'};
-  `;
 }
 
-// import ts from 'typescript';
-// function generateAliases({
-//   family,
-//   weight,
-//   style
-// }: {
-//   family: string[];
-//   weight: (string | number)[];
-//   style: string[];
-// }) {
-//   const sourceFile = ts.createSourceFile(
-//     'index.d.ts',
-//     '',
-//     ts.ScriptTarget.ESNext,
-//     false,
-//     ts.ScriptKind.TS
-//   );
+function getFromVariances<T>(fontConfig: FontConfig, key: keyof FontOptionVariance): T[] {
+  const uniqueStyles = new Set<T>()
+  const fontCount = fontConfig.fonts.length
 
-//   const updatedSourceFile = ts.factory.updateSourceFile(sourceFile, [
-//     createAliasDeclaration('FontFamily', family),
-//     createAliasDeclaration('FontWeigth', weight),
-//     createAliasDeclaration('FontStyle', style)
-//   ]);
+  for (let i = 0; i < fontCount; i++) {
+    const values = fontConfig.fonts[i].variances
+    for (let j = 0, valueCount = values.length; j < valueCount; j++) {
+      uniqueStyles.add(values[j][key] as T)
+    }
+  }
 
-//   const printer = ts.createPrinter();
-//   return printer.printFile(updatedSourceFile);
-// }
+  return Array.from(uniqueStyles).filter(Boolean)
+}
 
-// function createAliasDeclaration(name: string, values: (string | number)[]) {
-//   if (values.length === 0) {
-//     values = ['any'];
-//   }
-//   return ts.factory.createTypeAliasDeclaration(
-//     [ts.factory.createModifier(ts.SyntaxKind.ExportKeyword)],
-//     name,
-//     undefined,
-//     ts.factory.createUnionTypeNode(
-//       values.map(value => {
-//         let literal;
-//         if (typeof value === 'number') {
-//           literal = ts.factory.createNumericLiteral(String(value));
-//         } else {
-//           literal = ts.factory.createStringLiteral(String(value));
-//         }
-//         return ts.factory.createLiteralTypeNode(literal);
-//       })
-//     )
-//   );
-// }
+function unique<T>(array: T[]): T[] {
+  return Array.from(new Set(array)).filter(Boolean);
+}
+
+function capitalize(text: string): string {
+  return text ? `${text.charAt(0).toUpperCase()}${text.slice(1)}` : text;
+}
+
+function generateAliases(textProperties: TextProperties): string {
+  const props = Object.keys(textProperties);
+  const len = props.length;
+  const output: string[] = new Array(len);
+  // Loop through each text property
+  for (let i = 0; i < len; i++) {
+    const prop = props[i];
+    const propValues: (string | number)[] = textProperties[props[i]];
+    const valueCount = propValues.length;
+    let line = `export type Font${capitalize(prop)} = `;
+    // Output generic type "string", if there are no values to parse
+    if (!valueCount) output[i] = `${line}'string';`;
+    else {
+      // Merge all values into a union (|) type value.
+      const values: string[] = new Array(valueCount);
+      for (let j = 0; j < valueCount; j++) {
+        values[j] = `'${propValues[j]}'`;
+      }
+      output[i] = `${line}${values.join(' | ')};`;
+    }
+  }
+  return output.join('\n');
+}
+
+export function getTypesContent(fontConfig: FontConfig): string {
+  return generateAliases({
+    family: unique(fontConfig.fonts.map(font => font.family)),
+    weight: getFromVariances(fontConfig, 'weight'),
+    style: getFromVariances(fontConfig, 'style')
+  });
+}
